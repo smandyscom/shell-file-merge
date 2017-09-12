c8700e9c1840a35225788610b6afbbe305e4cf7e
smandyscom <smandyscom@gmail.com>
2017-09-12 23:07:00 +0800

    some progress

--- merger.sh
+++ merger.sh
@@ -1,18 +1,11 @@
 #!/bin/zsh
-#cd "$(dirname "$0")"
 
 echo $1; #input argument
 echo $2;
 
-#counter=0;
-#for i in $(ls $1) 
-#do
-#array[$counter]=$i
-#let counter++
-#done
-
 retval=-1 # as return value
 function get_file_list(){
+    unset retval
     retval=-1
     counter=0
     for i in $(ls $1);
@@ -22,6 +15,22 @@ function get_file_list(){
         done
 }
 
+#cache list1
 get_file_list $1
+list1=("${retval[@]}")
+#cache list2
+get_file_list $2
+list2=("${retval[@]}")
+
+#check which one is fewer
+if [ ${#list1[@]} -lt ${#list2[@]} ];then
+fewer=("${list1[@]}")
+other=("${list2[@]}")
+else
+fewer=("${list2[@]}")
+other=("${list1[@]}")
+fi
+
+#use fewer one as key to check if redundant in other directory
 
 echo ${retval[@]} #echo whole array
