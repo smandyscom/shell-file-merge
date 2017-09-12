2f3159cf1b7e6f911fe6a406d15e193b0cd003ae
smandyscom <smandyscom@gmail.com>
2017-09-12 23:26:22 +0800

    sort out redundant files

--- merger.sh
+++ merger.sh
@@ -15,12 +15,21 @@ function get_file_list(){
         done
 }
 
+#from https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+
 #cache list1
 get_file_list $1
 list1=("${retval[@]}")
 #cache list2
 get_file_list $2
-list2=("${retval[@]}")
+list2=("${retval[@]}") #copy of original array
 
 #check which one is fewer
 if [ ${#list1[@]} -lt ${#list2[@]} ];then
@@ -32,5 +41,15 @@ other=("${list1[@]}")
 fi
 
 #use fewer one as key to check if redundant in other directory
+for i in ${fewer[@]}
+    do
+        containsElement $i ${other[@]}
+        if [ $? -eq 0 ]
+        then
+        echo $i
+        redundant+=($i) #append element
+        fi
+    done
 
 echo ${retval[@]} #echo whole array
+echo ${redundant[@]}
