d6aa1823b34f2b99c71a5e35079bf28e3488c78f
smandyscom <smandyscom@gmail.com>
2017-09-12 22:20:29 +0800

    in function form

--- merger.sh
+++ merger.sh
@@ -4,11 +4,24 @@
 echo $1; #input argument
 echo $2;
 
-counter=0;
-for i in $(ls $1) 
-do
-array[$counter]=$i
-let counter++
-done
+#counter=0;
+#for i in $(ls $1) 
+#do
+#array[$counter]=$i
+#let counter++
+#done
 
-echo ${array[@]} #echo whole array
+retval=-1 # as return value
+function get_file_list(){
+    retval=-1
+    counter=0
+    for i in $(ls $1);
+        do
+        retval[$counter]=$i
+        let counter++
+        done
+}
+
+get_file_list $1
+
+echo ${retval[@]} #echo whole array
